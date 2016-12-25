827b4f071c17e7bf38f389c0769cb11fdeaf7113
genericlady <yiannakokalas@gmail.com>
2016-12-25 17:48:54 -0500

    Add mix.exs to package files inside of mix.exs package

--- mix.exs
+++ mix.exs
@@ -35,7 +35,7 @@ defmodule Chemistry.Mixfile do
 
   defp package do
     [
-      files: ~w(lib),
+      files: ~w(lib mix.exs),
       maintainers: ["Yianna Kokalas"],
       licenses: ["Apache 2.0"],
       links: %{"Github" => "https://github.com/genericlady/chemistry"}
