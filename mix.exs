9ba5b1992c92baedffe1888f74718e0230bcbee3
genericlady <yiannakokalas@gmail.com>
2016-12-25 11:44:50 -0500

    Add a short description to README, refactor installation instructions and add License info for Apache 2 and include an external link.

--- mix.exs
+++ mix.exs
@@ -1,32 +1,43 @@
 defmodule Chemistry.Mixfile do
   use Mix.Project
 
+  @version "0.1.0"
+
   def project do
-    [app: :chemistry,
-     version: "0.1.0",
-     elixir: "~> 1.3",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps()]
+    [
+      app: :chemistry,
+      version: @version,
+      elixir: "~> 1.3",
+      deps: deps(),
+      name: "Chemistry",
+      source_url: "https://github.com/genericlady/chemistry",
+      docs: [source_ref: "v#{@version}", main: "readme", extras: ["README.md"]],
+      description: description(),
+      package: package()
+    ]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
     [applications: [:logger]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [
+      {:ex_doc, "~> 0.12", only: :dev} 
+    ]
+  end
+
+  defp description do
+    """
+    Chemistry is a Testing Framework for your Elixir projects.
+    """
+  end
+
+  defp package do
+    [
+      maintainers: ["Yianna Kokalas"],
+      licenses: ["Apache 2.0"],
+      links: %{"Github" => "https://github.com/genericlady/chemistry"}
+    ]
   end
 end
